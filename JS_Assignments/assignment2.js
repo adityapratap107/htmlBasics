02c0089687e1f354ab0c217704bf22b5fd95fb8f
Aditya Pratap Singh <aditya.singh@netsolutions.com>
2023-07-22 22:02:07 +0530

    update assignment 17

--- JS_Assignments/assignment2.js
+++ JS_Assignments/assignment2.js
@@ -25,3 +25,45 @@
 // const countChars = (str, char) => str.split('').filter(ch => ch === char).length;
 // const contains2To4 = (str, char) => countChars(str, char) >= 2 && countChars(str,char) <=4;
 // console.log(contains2To4("aditya", 'a'))
+
+
+// 6. Write a javascript program to find the number of even digits in an array of integers
+// const countEvenCharacters = (arr) => arr.filter(num => num % 2 === 0).length;
+// console.log(countEvenCharacters([1,2,3,4,5,6,7,8]))
+
+
+// 7. write a javascript program to find the number of even values upto a given number
+// const countEvenCharacters = (arr) => arr.filter(num => num % 2 === 0).length;
+
+// const createArrayOfNumbers = (num) => {
+//     const returnArray = [];
+//     for(let i = 1; i <= num; i++) {
+//         returnArray.push(i);
+//     }
+//     return returnArray;
+// }
+// console.log(countEvenCharacters(createArrayOfNumbers(8)))
+
+
+// 8. Write a javascript program to check whether a given array of integers is sorted in ascending order
+// const isAscending = (arr) => {
+//     for(let i=0; i<arr.length; i++) {
+//         if(arr[i+1] < arr[i]) {
+//             return false
+//         }
+//     }
+//     return true;
+// }
+
+// console.log(isAscending([1,2,3,4,5,6]));
+// // console.log(isAscending([9,1,2,3,4,5,6]));
+
+
+// 9. Write a javascript program to get the largest even number from an array of integers using Math.max
+// const largestEvent = (arr) => Math.max(...arr.filter(num => num % 2 === 0));
+// console.log(largestEvent([1,2,3,4,5,6]));
+
+
+// 10. Write a javascript program to replace the first digit in a string (should contain at least digit) with $ character
+const replaceFirstDigit = (str) => str.replace(/[0-9]/, '$')
+console.log(replaceFirstDigit('Abcccg1gjde'))
