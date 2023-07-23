aad84e2f0421a01ebcc66d31680f3cd17a645cd9
Aditya Pratap Singh <aditya.singh@netsolutions.com>
2023-07-23 21:56:01 +0530

    updated

--- JS_Assignments/assignment3.js
+++ JS_Assignments/assignment3.js
@@ -19,14 +19,90 @@
 
 
 // 3. generate a random hexadecimal color code
-const getRandomHexNumber = () => Math.floor(Math.random() * 16).toString(16);  // because we want base 16 numbers
-const getRandomHexColor = () => '#' + Array.from({length: 6}).map(getRandomHexNumber).join('')
+// const getRandomHexNumber = () => Math.floor(Math.random() * 16).toString(16);  // because we want base 16 numbers
+// const getRandomHexColor = () => '#' + Array.from({length: 6}).map(getRandomHexNumber).join('')
 
-console.log(getRandomHexColor());
-console.log(getRandomHexColor());
-console.log(getRandomHexColor());
-console.log(getRandomHexColor());
-console.log(getRandomHexColor());
+// console.log(getRandomHexColor());
+// console.log(getRandomHexColor());
+// console.log(getRandomHexColor());
+// console.log(getRandomHexColor());
+// console.log(getRandomHexColor());
+
+
+// 4. Write a javascript function that returns a passed string with letters in alphabetical order
+// Example string: 'webmaster'
+// Expected Output: 'abeemrstw'
+// const alphabeticalOrder = (str) => str.split('').sort((a,b) => a > b ? 1 : -1).join('')
+// console.log(alphabeticalOrder('webmaster'))
+
+
+// 5. Write a javascript function that accepts a string as a parameter and counts the number of vowels within the string.
+// const countVowels = (str) => {
+//     let vowels = ['a', 'e', 'i', 'o', 'u'];
+//     return str.split('').filter(char => vowels.indexOf(char) > -1).length
+    
+
+// }
+// console.log(countVowels("aditya"))
+
+
+// 6. write a javascript function to get the unique characters from string
+// const findUnique = (str) => {
+//     let uniqueStr = "";
+//     for(let i=0; i<str.length; i++) {
+//         if(uniqueStr.includes(str[i]) === false) {
+//             uniqueStr += str[i]
+//         }
+//     }
+//     return uniqueStr
+// };
+// console.log(findUnique("aabbccd"));
+
+// using set
+// const findUnique = (str) =>{
+//     const uniqueCharSet = new Set();
+//     for(const char of str) {
+//         uniqueCharSet.add(char)
+//     }
+//     return Array.from(uniqueCharSet).join('')
+// } 
+// console.log(findUnique("aabbccc"));
+
+
+// 7. Write a javascript function to find the first not repeated character
+// Example string: 'abacddbec'
+// Expected output: 'e'
+
+
+// get non repeated characters
+// const getNonRepeatedCharacters = (str) => str.split("").filter((item, index, arr) => arr.filter(arrItem => arrItem ===item).length ===1)
+// console.log(getNonRepeatedCharacters("abacddbec"))
+
+
+// logic to a transformToObjects() function that should transform a list of numbers into a list of JavaScript objects.
+// 
+// function transformToObjects(numberArray) {
+//     // Todo: Add your logic
+//     // should return an array of objects
+    
+//     return numberArray.map(num => {
+//       return {val: num}  
+//     })
+// }
+
+// console.log(transformToObjects([1,2,3]))
 
 
 
+// array destructuring
+// const [firstName, lastName] = ["myName", "lastmyName"];
+// console.log(firstName)
+
+// object destructuring
+const user = {
+    name: "abc",
+    age: 31,
+};
+
+const {name: firstName, age: myAge} = user;
+console.log(firstName, myAge)
